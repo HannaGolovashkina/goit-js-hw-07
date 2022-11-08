93077af7313862e33542a475366ccf61b63da1c8
Hanna Golovashkina <108117285+HannaGolovashkina@users.noreply.github.com>
2022-11-08 12:09:19 +0200

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const imgContainer = document.querySelector(".gallery");
+const imgMarkup = createImgMarkup(galleryItems);
+
+imgContainer.insertAdjacentHTML("beforeend", imgMarkup);
+
+imgContainer.addEventListener("click", onImgContainerClick);
+
+function createImgMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join("");
+}
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionType: 250,
+  /* options */
+});
+function onImgContainerClick(event) {
+  event.preventDefault();
+}
 
 console.log(galleryItems);
